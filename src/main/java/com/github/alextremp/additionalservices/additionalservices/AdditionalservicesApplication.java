b1cab1673acd0be5370eb10b876508b4789065ef
Alex Castells <alextremp@hotmail.com>
2019-10-18 12:37:53 +0200

    first commit

---
+++ src/main/java/com/github/alextremp/additionalservices/additionalservices/AdditionalservicesApplication.java
@@ -0,0 +1,13 @@
+package com.github.alextremp.additionalservices.additionalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionalservicesApplication.class, args);
+	}
+
+}
